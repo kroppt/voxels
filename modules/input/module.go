c2a372ef849f96cb691818da3da368a975b31cd5
Tyler Kropp <tyler@tylerkropp.xyz>
2021-09-18 13:39:27 -0400

    Define dependencies of input next to constructor

--- modules/input/module.go
+++ modules/input/module.go
@@ -1,8 +1,27 @@
 package input
 
+import (
+	"github.com/kroppt/voxels/modules/player"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
 // Module is a synchronous input router.
 type Module struct {
 	c core
+}
+type graphicsMod interface {
+	DestroyWindow() error
+	PollEvent() (sdl.Event, bool)
+}
+
+type playerMod interface {
+	HandleMovementEvent(player.MovementEvent)
+	HandleLookEvent(player.LookEvent)
+}
+
+type settingsRepo interface {
+	GetFOV() float64
+	GetResolution() (uint32, uint32)
 }
 
 // New creates a synchronous input module.
