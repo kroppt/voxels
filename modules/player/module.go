4ccc03f80104e4e5600c3cf72ad4f70de7ca3313
Tyler Kropp <tyler@tylerkropp.xyz>
2021-09-18 13:40:06 -0400

    Define dependencies of player next to constructor

--- modules/player/module.go
+++ modules/player/module.go
@@ -1,8 +1,21 @@
 package player
 
+import (
+	"github.com/kroppt/voxels/modules/chunk"
+	"github.com/kroppt/voxels/modules/graphics"
+)
+
 // Module is a player.
 type Module struct {
 	c core
+}
+
+type chunkMod interface {
+	UpdatePosition(chunk.PositionEvent)
+}
+
+type graphicsMod interface {
+	UpdateDirection(graphics.DirectionEvent)
 }
 
 // New creates a player.
