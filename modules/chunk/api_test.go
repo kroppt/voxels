3f4d1692886e0d76c504e2e23a5e84501a8407e3
Tyler Kropp <tyler@tylerkropp.xyz>
2021-10-11 22:24:32 -0400

    Add tests for initial load to be for a chunk

--- modules/chunk/api_test.go
+++ modules/chunk/api_test.go
@@ -1,6 +1,7 @@
 package chunk_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/kroppt/voxels/modules/chunk"
@@ -29,27 +30,65 @@ func TestModuleNew(t *testing.T) {
 			},
 		}
 
-		mod := chunk.New(graphicsMod)
+		mod := chunk.New(graphicsMod, 1)
 
 		if mod == nil {
 			t.Fatal("expected non-nil return")
 		}
 	})
 
-	t.Run("calls ShowVoxel", func(t *testing.T) {
-		var evt *graphics.VoxelEvent
+	t.Run("calls ShowVoxel based on chunk size", func(t *testing.T) {
+		testCases := []struct {
+			chunkSize uint
+			expect    uint
+		}{
+			{
+				chunkSize: 1,
+				expect:    1 * 1,
+			},
+			{
+				chunkSize: 2,
+				expect:    2 * 2,
+			},
+			{
+				chunkSize: 3,
+				expect:    3 * 3,
+			},
+		}
+		for _, tC := range testCases {
+			var calls uint
+			t.Run(fmt.Sprintf("called %v times for chunk size %v", calls, tC.chunkSize), func(t *testing.T) {
+				graphicsMod := fnGraphicsMod{
+					fnUpdateDirection: func(graphics.DirectionEvent) {
+					},
+					fnShowVoxel: func(graphics.VoxelEvent) {
+						calls++
+					},
+				}
+
+				_ = chunk.New(graphicsMod, tC.chunkSize)
+
+				if calls != tC.expect {
+					t.Fatalf("expected %v calls but got %v", tC.expect, calls)
+				}
+			})
+		}
+	})
+
+	t.Run("zero chunk size causes panic", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Fatal("expected panic but got none")
+			}
+		}()
+
 		graphicsMod := fnGraphicsMod{
 			fnUpdateDirection: func(graphics.DirectionEvent) {
 			},
-			fnShowVoxel: func(voxelEvent graphics.VoxelEvent) {
-				evt = &voxelEvent
+			fnShowVoxel: func(graphics.VoxelEvent) {
 			},
 		}
 
-		_ = chunk.New(graphicsMod)
-
-		if evt == nil {
-			t.Fatal("expected function to be called")
-		}
+		_ = chunk.New(graphicsMod, 0)
 	})
 }
--- modules/chunk/module.go
+++ modules/chunk/module.go
@@ -6,11 +6,15 @@ type Module struct {
 }
 
 // New creates a chunk module.
-func New(graphicsMod graphicsMod) *Module {
+func New(graphicsMod graphicsMod, chunkSize uint) *Module {
+	if chunkSize == 0 {
+		panic("chunk size cannot be 0")
+	}
+
 	core := core{
 		graphicsMod: graphicsMod,
 	}
-	core.init()
+	core.init(chunkSize)
 	return &Module{
 		core,
 	}
