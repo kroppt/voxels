6b14d91b7bafebf8bbb9ac209a97551fe5a8845e
Greg Johnson <gregjohnson2017@gmail.com>
2021-12-19 20:31:30 -0600

    Add input.Interface and function stub

--- modules/input/api.go
+++ modules/input/api.go
@@ -1,5 +1,10 @@
 package input
 
+type Interface interface {
+	RouteEvents()
+	PixelsToRadians(xRel, yRel int32) (float64, float64)
+}
+
 // RouteEvents polls for input events and routes them to other modules.
 func (m *Module) RouteEvents() {
 	m.c.routeEvents()
@@ -8,4 +13,22 @@ func (m *Module) RouteEvents() {
 // PixelsToRadians converts from pixels to radians in terms of camera rotation.
 func (m *Module) PixelsToRadians(xRel, yRel int32) (float64, float64) {
 	return m.c.pixelsToRadians(xRel, yRel)
+}
+
+type FnModule struct {
+	FnRouteEvents     func()
+	FnPixelsToRadians func(xRel, yRel int32) (float64, float64)
+}
+
+func (fn *FnModule) RouteEvents() {
+	if fn.FnRouteEvents != nil {
+		fn.FnRouteEvents()
+	}
+}
+
+func (fn *FnModule) PixelsToRadians(xRel, yRel int32) (float64, float64) {
+	if fn.FnPixelsToRadians != nil {
+		return fn.FnPixelsToRadians(xRel, yRel)
+	}
+	return 0, 0
 }
