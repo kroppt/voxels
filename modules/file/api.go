7e26eac2e6076fdabafcb882e75f28906cd2c800
Greg Johnson <gregjohnson2017@gmail.com>
2021-12-19 20:35:41 -0600

    Add file.Interface and function stub

--- modules/file/api.go
+++ modules/file/api.go
@@ -2,7 +2,22 @@ package file
 
 import "io"
 
+type Interface interface {
+	GetReadCloser(fileName string) (io.ReadCloser, error)
+}
+
 // GetReadCloser returns an io.ReadCloser of the specified file if the file name is valid.
 func (m *Module) GetReadCloser(fileName string) (io.ReadCloser, error) {
 	return m.c.getReadCloser(fileName)
+}
+
+type FnModule struct {
+	FnGetReadCloser func(fileName string) (io.ReadCloser, error)
+}
+
+func (fn *FnModule) GetReadCloser(fileName string) (io.ReadCloser, error) {
+	if fn.FnGetReadCloser != nil {
+		return fn.FnGetReadCloser(fileName)
+	}
+	return nil, nil
 }
