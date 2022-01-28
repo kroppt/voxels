aa0d23448a9a93a6897438b79b68abbb6e623d10
Greg Johnson <gregjohnson2017@gmail.com>
2022-01-27 18:31:23 -0600

    Rename chunk->player in comment

--- modules/player/module.go
+++ modules/player/module.go
@@ -5,12 +5,12 @@ import (
 	"github.com/kroppt/voxels/repositories/settings"
 )
 
-// Module is a chunk module.
+// Module is a player module.
 type Module struct {
 	c core
 }
 
-// New creates a chunk module.
+// New creates a player module.
 func New(worldMod world.Interface, settingsMod settings.Interface, chunkSize uint32) *Module {
 	if settingsMod == nil {
 		panic("settings is required to be non-nil")
