f0771925334bc6f86df4b9cf680c1ee8b57e8457
Greg Johnson <gregjohnson2017@gmail.com>
2022-01-29 00:18:35 -0600

    Fix unkeyed fields

--- modules/world/api_test.go
+++ modules/world/api_test.go
@@ -12,11 +12,7 @@ import (
 func TestWorldUpdateChunkView(t *testing.T) {
 	t.Parallel()
 	expectedViewableChunks := map[chunk.Position]struct{}{
-		{
-			X: 1,
-			Y: 2,
-			Z: 3,
-		}: {},
+		{X: 1, Y: 2, Z: 3}: {},
 	}
 	var actualViewableChunks map[chunk.Position]struct{}
 	graphicsMod := graphics.FnModule{
@@ -26,7 +22,7 @@ func TestWorldUpdateChunkView(t *testing.T) {
 	}
 	worldMod := world.New(graphicsMod, nil)
 	worldMod.UpdateView(map[chunk.Position]struct{}{
-		{1, 2, 3}: {},
+		{X: 1, Y: 2, Z: 3}: {},
 	})
 
 	if !reflect.DeepEqual(expectedViewableChunks, actualViewableChunks) {
@@ -49,51 +45,51 @@ func TestWorldLoadedChunkCount(t *testing.T) {
 		{
 			desc: "world should load one chunk",
 			loadChunks: []chunk.Position{
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			expectedCount: 1,
 		},
 		{
 			desc: "world should load and unload the same chunk",
 			loadChunks: []chunk.Position{
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			unloadChunks: []chunk.Position{
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			expectedCount: 0,
 		},
 		{
 			desc: "world should not load the same chunk twice",
 			loadChunks: []chunk.Position{
-				{1, 2, 3},
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			expectedCount: 1,
 		},
 		{
 			desc: "world should not unload the same chunk twice",
 			loadChunks: []chunk.Position{
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			unloadChunks: []chunk.Position{
-				{1, 2, 3},
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			expectedCount: 0,
 		},
 		{
 			desc: "world should load two different chunks",
 			loadChunks: []chunk.Position{
-				{1, 2, 3},
-				{4, 5, 6},
+				{X: 1, Y: 2, Z: 3},
+				{X: 4, Y: 5, Z: 6},
 			},
 			expectedCount: 2,
 		},
 		{
 			desc: "world cannot unload a chunk if it has none",
 			unloadChunks: []chunk.Position{
-				{1, 2, 3},
+				{X: 1, Y: 2, Z: 3},
 			},
 			expectedCount: 0,
 		},
@@ -128,12 +124,8 @@ func TestWorldLoadChunkPassesToGraphics(t *testing.T) {
 	}
 
 	worldMod := world.New(graphicsMod, &world.FnGenerator{})
-	worldMod.LoadChunk(chunk.Position{1, 2, 3})
-	expected := chunk.Position{
-		X: 1,
-		Y: 2,
-		Z: 3,
-	}
+	worldMod.LoadChunk(chunk.Position{X: 1, Y: 2, Z: 3})
+	expected := chunk.Position{X: 1, Y: 2, Z: 3}
 	if actual != expected {
 		t.Fatalf("expected graphics to receive %v but got %v", expected, actual)
 	}
@@ -148,12 +140,8 @@ func TestWorldUnloadChunkPassesToGraphics(t *testing.T) {
 		},
 	}
 	worldMod := world.New(graphicsMod, nil)
-	worldMod.UnloadChunk(chunk.Position{1, 2, 3})
-	expected := chunk.Position{
-		X: 1,
-		Y: 2,
-		Z: 3,
-	}
+	worldMod.UnloadChunk(chunk.Position{X: 1, Y: 2, Z: 3})
+	expected := chunk.Position{X: 1, Y: 2, Z: 3}
 	if actual != expected {
 		t.Fatalf("expected graphics to receive %v but got %v", expected, actual)
 	}
@@ -163,16 +151,8 @@ func TestWorldGeneration(t *testing.T) {
 	t.Parallel()
 	testGen := &world.FnGenerator{
 		FnGenerateChunk: func(_ chunk.Position) chunk.Chunk {
-			newChunk := chunk.New(chunk.Position{
-				X: 0,
-				Y: 0,
-				Z: 0,
-			}, 1)
-			newChunk.SetBlockType(chunk.VoxelCoordinate{
-				X: 0,
-				Y: 0,
-				Z: 0,
-			}, chunk.BlockTypeDirt)
+			newChunk := chunk.New(chunk.Position{X: 0, Y: 0, Z: 0}, 1)
+			newChunk.SetBlockType(chunk.VoxelCoordinate{X: 0, Y: 0, Z: 0}, chunk.BlockTypeDirt)
 			return newChunk
 		},
 	}
@@ -180,15 +160,11 @@ func TestWorldGeneration(t *testing.T) {
 	var actual chunk.BlockType
 	graphicsMod := graphics.FnModule{
 		FnLoadChunk: func(ch chunk.Chunk) {
-			actual = ch.BlockType(chunk.VoxelCoordinate{
-				X: 0,
-				Y: 0,
-				Z: 0,
-			})
+			actual = ch.BlockType(chunk.VoxelCoordinate{X: 0, Y: 0, Z: 0})
 		},
 	}
 	worldMod := world.New(graphicsMod, testGen)
-	worldMod.LoadChunk(chunk.Position{0, 0, 0})
+	worldMod.LoadChunk(chunk.Position{X: 0, Y: 0, Z: 0})
 
 	if actual != expected {
 		t.Fatalf("expected to retrieve block type %v but got %v", expected, actual)
